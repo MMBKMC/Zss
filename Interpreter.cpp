27ca9de092f526deaa50344e361a3991edeb4247
MMBKMC <151632121+MMBKMC@users.noreply.github.com>
2024-03-29 19:49:33 +0630

    Add files via upload

---
+++ Interpreter.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+enum class TokenType {
+    INTEGER,
+    PLUS,
+    MINUS,
+    PRINT,
+    MAIN,
+    PYTHON,
+    IMPORT,
+    INPUT,
+    EOF_TOKEN
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+};
+
+class Lexer {
+public:
+    Lexer(std::string text) : text(text), pos(0) {}
+
+    Token getNextToken() {
+        if (pos >= text.size())
+            return Token{ TokenType::EOF_TOKEN, "" };
+
+        if (isdigit(text[pos])) {
+            std::string num;
+            while (pos < text.size() && isdigit(text[pos])) {
+                num.push_back(text[pos]);
+                pos++;
+            }
+            return Token{ TokenType::INTEGER, num };
+        }
+
+        if (text[pos] == '+') {
+            pos++;
+            return Token{ TokenType::PLUS, "+" };
+        }
+
+        if (text[pos] == '-') {
+            pos++;
+            return Token{ TokenType::MINUS, "-" };
+        }
+
+        // Invalid token
+        throw std::runtime_error("Invalid character");
+    }
+
+private:
+    std::string text;
+    size_t pos;
+};
+
+class Parser {
+public:
+    Parser(Lexer& lexer) : lexer(lexer), currentToken(lexer.getNextToken()) {}
+
+    void eat(TokenType expectedType) {
+        if (currentToken.type == expectedType) {
+            currentToken = lexer.getNextToken();
+        }
+        else {
+            throw std::runtime_error("Invalid syntax");
+        }
+    }
+
+    int factor() {
+        Token token = currentToken;
+        eat(TokenType::INTEGER);
+        return std::stoi(token.value);
+    }
+
+    int expr() {
+        int result = factor();
+
+        while (currentToken.type == TokenType::PLUS || currentToken.type == TokenType::MINUS) {
+            Token op = currentToken;
+            if (op.type == TokenType::PLUS) {
+                eat(TokenType::PLUS);
+                result += factor();
+            }
+            else {
+                eat(TokenType::MINUS);
+                result -= factor();
+            }
+        }
+
+        return result;
+    }
+
+    int parse() {
+        return expr();
+    }
+
+private:
+    Lexer& lexer;
+    Token currentToken;
+};
+
+int main() {
+    std::string input = "10 + 5 - 3";
+    Lexer lexer(input);
+    Parser parser(lexer);
+
+    try {
+        int result = parser.parse();
+        std::cout << "Result: " << result << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
+#include <fstream>
+
+int main() {
+    std::ifstream inputFile("test.zss");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    std::string input;
+    std::getline(inputFile, input);
+    inputFile.close();
+
+    Lexer lexer(input);
+    Interpreter interpreter(lexer);
+
+    try {
+        int result = interpreter.interpret();
+        std::cout << "Result: " << result << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ Lexer.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    INTEGER,
+    PLUS,
+    MINUS,
+    EOF_TOKEN
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+};
+
+class Lexer {
+public:
+    Lexer(std::string text) : text(text), pos(0) {}
+
+    Token getNextToken() {
+        if (pos >= text.size())
+            return Token{TokenType::EOF_TOKEN, ""};
+
+        if (isdigit(text[pos])) {
+            std::string num;
+            while (pos < text.size() && isdigit(text[pos])) {
+                num.push_back(text[pos]);
+                pos++;
+            }
+            return Token{TokenType::INTEGER, num};
+        }
+
+        if (text[pos] == '+') {
+            pos++;
+            return Token{TokenType::PLUS, "+"};
+        }
+
+        if (text[pos] == '-') {
+            pos++;
+            return Token{TokenType::MINUS, "-"};
+        }
+
+        // Invalid token
+        throw std::runtime_error("Invalid character");
+    }
+
+private:
+    std::string text;
+    size_t pos;
+};
+
+int main() {
+    std::string input = "10 + 5 - 3";
+    Lexer lexer(input);
+
+    Token token;
+    while ((token = lexer.getNextToken()).type != TokenType::EOF_TOKEN) {
+        std::cout << "Token: " << token.value << std::endl;
+    }
+
+    return 0;
+}
